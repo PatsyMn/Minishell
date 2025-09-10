52c6169d26d4ae420a7c70480352d282a8a96615
patsyMn <patsy.meimoun@laposte.net>
2025-09-10 14:45:25 +0200

    empty  ok

--- parsing/lexer_word.c
+++ parsing/lexer_word.c
@@ -6,39 +6,104 @@
 /*   By: pmeimoun <pmeimoun@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/02 10:46:14 by pmeimoun          #+#    #+#             */
-/*   Updated: 2025/09/02 11:04:34 by pmeimoun         ###   ########.fr       */
+/*   Updated: 2025/09/10 14:43:17 by pmeimoun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-int	add_word_token(char *str, int start, int *i, t_token **token_list)
+static char	*handle_quoted_word(char *str, int *i)
 {
+	char	quote;
+	int		start;
 	char	*sub;
 
-	sub = ft_substr(str, start, *i - start);
-	if (!sub)
-		return (0);
-	add_token_to_list(token_list, create_token(get_token_type_from_str(sub),
-			sub));
-	return (1);
-}
-
-int	handle_word(char *str, int *i, t_token **token_list)
-{
-	int	start;
-
+	quote = str[*i];
 	start = *i;
-	while (str[*i])
+	(*i)++;
+	while (str[*i] && str[*i] != quote)
 	{
-		if (operator_length(&str[*i]) > 0)
-		{
-			return (add_word_token(str, start, i, token_list));
-		}
 		if (str[*i] == '\\' && str[*i + 1])
 			*i += 2;
 		else
 			(*i)++;
 	}
-	return (add_word_token(str, start, i, token_list));
+	if (str[*i] != quote)
+		return (NULL);
+	(*i)++;
+	sub = ft_substr(str, start + 1, *i - start - 2);
+	return (sub);
 }
+
+static char	*append_and_free(char *s1, char *s2)
+{
+	char	*new_str;
+
+	new_str = ft_strjoin(s1, s2);
+	free(s1);
+	free(s2);
+	return (new_str);
+}
+
+static int	handle_unquoted_word(char *str, int *i, char **buffer)
+{
+	int		start;
+	char	*tmp;
+
+	start = *i;
+	while (str[*i] && str[*i] != '\'' && str[*i] != '"' &&
+			str[*i] != ' ' && operator_length(&str[*i]) == 0)
+	{
+		if (str[*i] == '\\' && str[*i + 1])
+			*i += 2;
+		else
+			(*i)++;
+	}
+	tmp = ft_substr(str, start, *i - start);
+	if (!tmp)
+		return (0);
+	*buffer = append_and_free(*buffer, tmp);
+	return (1);
+}
+
+static int	build_word_buffer(char *str, int *i, char **buffer)
+{
+	char	*tmp;
+
+	while (str[*i] && str[*i] != ' ' && operator_length(&str[*i]) == 0)
+	{
+		if (str[*i] == '\'' || str[*i] == '"')
+		{
+			tmp = handle_quoted_word(str, i);
+			if (!tmp)
+				return (0);
+			*buffer = append_and_free(*buffer, tmp);
+		}
+		else
+		{
+			if (!handle_unquoted_word(str, i, buffer))
+				return (0);
+		}
+	}
+	return (1);
+}
+
+int	handle_word(char *str, int *i, t_token **token_list)
+{
+	char	*buffer;
+	int		ret;
+
+	buffer = ft_strdup("");
+	if (!buffer)
+		return (0);
+	ret = build_word_buffer(str, i, &buffer);
+	if (ret)
+	{
+		add_token_to_list(token_list, create_token(T_WORD, buffer));
+		ret = 1;
+	}
+	return (ret);
+}
+
+
+
