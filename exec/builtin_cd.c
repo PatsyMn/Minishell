98c6b9696fb7beb5ab3e9ebee3b21ae7edcb750a
mbores06 <mbores@student.42nice.fr>
2025-09-30 16:20:21 +0200

    cd norme

--- exec/builtin_cd.c
+++ exec/builtin_cd.c
@@ -6,43 +6,48 @@
 /*   By: mbores <mbores@student.42nice.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/25 12:58:36 by mbores            #+#    #+#             */
-/*   Updated: 2025/09/25 16:42:28 by mbores           ###   ########.fr       */
+/*   Updated: 2025/09/30 16:18:29 by mbores           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
+static int cd_home(t_env **env, char *oldpwd)
+{
+    char    *home;
+
+    home = my_getenv(*env, "HOME");
+    if (!home)
+    {
+        write(2, "bash: cd: HOME not set\n", 22);
+        free(oldpwd);
+        return (1);
+    }
+    else if (chdir(home) == -1)
+    {
+        perror("bash: cd");
+        free(oldpwd);
+        return (1);
+    }
+    return (0);
+}
+
 int builtin_cd(t_command *command, t_env **env)
 {
     char    *oldpwd;
     char    *newpwd;
-    char    *home;
 
     oldpwd = getcwd(NULL, 0);
     if (!command->args[1])
     {
-        home = my_getenv(*env, "HOME");
-        if (!home)
-        {
-            write(2, "bash: cd: HOME not set\n", 22);
-            free(oldpwd);
+        if (cd_home(env, oldpwd))
             return (1);
-        }
-        if (chdir(home) == -1)
-        {
-            perror("bash: cd");
-            free(oldpwd);
-            return (1);
-        }
     }
-    else
+    else if (chdir(command->args[1]) == -1)
     {
-        if (chdir(command->args[1]) == -1)
-        {
-            perror("bash: cd");
-            free(oldpwd);
-            return (1);
-        }
+        perror("bash: cd");
+        free(oldpwd);
+        return (1);
     }
     newpwd = getcwd(NULL, 0);
     if (oldpwd)
