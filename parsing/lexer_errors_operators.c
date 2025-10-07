01f4c191ac50628ad8563bf4205208231b770c9a
Patsy Meimoun <pmeimoun@c1r2p4.42nice.fr>
2025-10-07 15:14:11 +0200

    && error ok

--- parsing/lexer_errors_operators.c
+++ parsing/lexer_errors_operators.c
@@ -6,7 +6,7 @@
 /*   By: pmeimoun <pmeimoun@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/18 16:27:33 by pmeimoun          #+#    #+#             */
-/*   Updated: 2025/10/02 16:04:02 by pmeimoun         ###   ########.fr       */
+/*   Updated: 2025/10/07 15:13:19 by pmeimoun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,7 @@ static int	is_operator(char *token)
 {
 	if (!ft_strncmp(token, ">>", 3) || !ft_strncmp(token, "<<", 3)
 		|| !ft_strncmp(token, ">", 2) || !ft_strncmp(token, "<", 2)
-		|| !ft_strncmp(token, "|", 2) || !ft_strncmp(token, "&&", 3)
-		|| !ft_strncmp(token, "<>", 3))
+		|| !ft_strncmp(token, "|", 2) || !ft_strncmp(token, "<>", 3))
 		return (1);
 	return (0);
 }
